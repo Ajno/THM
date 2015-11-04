5181690528e6291a97083cf999ebdc2206fe43ef
Jano <faber.jan@siemens.com>
2015-11-04 15:24:38 +0100

    THM library added

---
+++ base/Project_Headers/thm_lib.h
@@ -0,0 +1,21 @@
+/*
+ * thm_lib.h
+ *
+ *  Created on: Nov 4, 2015
+ *      Author: z0037ysj
+ */
+
+#ifndef THM_LIB_H_
+#define THM_LIB_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdtypes.h>
+
+schar* thmLibItoa(const sWord cNum, schar buffer[], const sWord cBase);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* THM_LIB_H_ */
---
+++ base/Sources/thm_lib.c
@@ -0,0 +1,14 @@
+/*
+ * thm_lib.c
+ *
+ *  Created on: Nov 4, 2015
+ *      Author: z0037ysj
+ */
+
+#include "thm_lib.h"
+
+schar* thmLibItoa(const sWord cNum, schar buffer[], const sWord cBase)
+{
+    // todo
+    return buffer;
+}

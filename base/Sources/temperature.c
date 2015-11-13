638706dbf2e73ce4a43216f00f4f3e63c50c724c
Jano <faber.jan@siemens.com>
2015-11-13 14:07:44 +0100

    temperature is more precise

--- base/Sources/temperature.c
+++ base/Sources/temperature.c
@@ -8,11 +8,17 @@
 #include "adc.h"
 #include "temperature.h"
 
-static const sWord cScale = 10;
-static const sWord cQ = -5000;
-static const sWord cK = 32;
+static const sWord cK1 = 3;
+static const sWord cK2 = 2;
+static const sWord cK3 = 3;
+static const sWord cQ1 = -500;
+static const sWord cQ2 = -50;
+static const sWord cQ3 = -5;
+static const sWord cScale2 = 10;
+static const sWord cScale3 = 100;
 
 /*
+ * temperature in degrees = number*k + q
  * k = 0,32258065
  * q = -50
  * 
@@ -20,8 +26,10 @@ static const sWord cK = 32;
  */
 sWord temperatureRead()
 {
-    sWord ret = (cK * adcChannelSample(cAdcChnnl_A3) + cQ);
-    return (ret/cScale);
+    sWord tempRaw = adcChannelSample(cAdcChnnl_A3);
+    sWord ret = 0;
+    ret = cK1 * tempRaw + cQ1 + (cK2 * tempRaw + cQ2)/cScale2 + (cK3 * tempRaw + cQ3)/cScale3;
+    return ret;
 }
 
 void temperatureInit()

6e17a33bd94143e2c932731bc468ce25e041c1cd
z0037ysj <z0037ysj@MD16EDQC.ww300.siemens.net>
2016-01-29 14:36:26 +0100

    refactoring

--- app/Sources/humidity.c
+++ app/Sources/humidity.c
@@ -10,22 +10,22 @@
 
 /*
  * f[Hz] vs RH[%] - for 25 deg
- * {f,quotient,offset,scale_1,scale_2}
- * RH=(quotient*f/scale_1+offset)/scale_2
+ * {f,quotient,offset,cScale1}
+ * RH=(quotient*f/cScale1+offset)/cScale2
  */
-static Word freqVsHum[10][5] =
+static Word freqVsHum[10][4] =
 {
-{ 21, 780, 167, 491, 1 }, //(0;20)
-{ 84, 780, 167, 491, 1 }, //(20;30) quot=1.5873015873, off=166.666666667
-{ 300, 215, 261, 464, 1 }, //(30;40) quot=0.4629629629, off=261.11111113
-{ 1050, 62, 360, 465, 1 }, //(40;50) quot=0.1333333333, off=360
-{ 1750, 32, 425, 448, 1 }, //(50;55) quot=0.0714285714, off=425.00000005
-{ 3232, 11, 491, 326, 1 }, //(55;60) qout=0.0337381916, off=490.958164749
-{ 9549, 6, 549, 379, 1 }, //(60;70) qout=0.0158302991, off=548.836473009
-{ 21008, 3, 617, 344, 1 }, //(70;80) qout=0.0087267649, off=616.668121128
-{ 45670, 1, 715, 247, 1 }, //(80;90) qout=0.0040548211, off=714.8163166
-{ 61789, 1, 758, 322, 1 } }; //(90;100) qout=0.0031019294, off=758.334884298
-
+{ 21, 780, 167, 491}, //(0;20)
+{ 84, 780, 167, 491 }, //(20;30) quot=1.5873015873, off=166.666666667
+{ 300, 215, 261, 464 }, //(30;40) quot=0.4629629629, off=261.11111113
+{ 1050, 62, 360, 465 }, //(40;50) quot=0.1333333333, off=360
+{ 1750, 32, 425, 448 }, //(50;55) quot=0.0714285714, off=425.00000005
+{ 3232, 11, 491, 326 }, //(55;60) qout=0.0337381916, off=490.958164749
+{ 9549, 6, 549, 379 }, //(60;70) qout=0.0158302991, off=548.836473009
+{ 21008, 3, 617, 344 }, //(70;80) qout=0.0087267649, off=616.668121128
+{ 45670, 1, 715, 247 }, //(80;90) qout=0.0040548211, off=714.8163166
+{ 61789, 1, 758, 322 } }; //(90;100) qout=0.0031019294, off=758.334884298
+static const Word cScale2 = 10;
 /*
  * t[10xdeg] vs RH[%] error relative to reference 25 deg
  */
@@ -105,9 +105,9 @@ static Word frequency2humidity(const Word frequency)
         }
     }
 
-    // RH=(frequency*quotient/scale_1+offset)/scale_2
+    // RH=(frequency*quotient/cScale1+offset)/scale_2
     return ((frequency * freqVsHum[idx][1]) / freqVsHum[idx][3]
-            + freqVsHum[idx][2]) / 10;
+            + freqVsHum[idx][2]) / cScale2;
 }
 
 static void compensate4Temperature(const sWord cTemperature, Word* pHumidity)
